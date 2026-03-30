9940903004eb25a89856d52df16f664b86efce26
João Arthur Birkheuer de Cristo <joaobirkheuer@proton.me>
2026-03-30 09:29:06 -0300

    feat: initial project structure

---
+++ src/main/java/com/servicosweb/apirest/joao/Application.java
@@ -0,0 +1,13 @@
+package com.servicosweb.apirest.joao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
